3bf1aca7753404097a7b36b4a8dc3de8756380fb
asif-kamal <asif.kamal1489@gmail.com>
2023-05-14 17:19:59 -0400

    Created subclass through inheritance

--- Dwelling_2.kt
+++ Dwelling_2.kt
@@ -1,5 +1,10 @@
 fun main() {
-    
+    val squareCabin = SquareCabin(6)
+
+    println("\nSquare Cabin\n===============")
+    println("Capacity: ${squareCabin.capacity}")
+    println("Material: ${squareCabin.buildingMaterial}")
+    println("Has room? ${squareCabin.hasRoom()}")
 }
 
 abstract class Dwelling(private var residents: Int) {
@@ -9,4 +14,9 @@ abstract class Dwelling(private var residents: Int) {
     fun hasRoom(): Boolean {
         return residents < capacity
     }
+}
+
+class SquareCabin(residents: Int) : Dwelling(residents) {
+    override val buildingMaterial = "Wood"
+    override val capacity = 6
 }
