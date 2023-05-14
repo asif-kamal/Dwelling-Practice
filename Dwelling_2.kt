0e5bd501c0d1f73410f30d982baa3a305213f0ba
asif-kamal <asif.kamal1489@gmail.com>
2023-05-14 17:06:00 -0400

    first commit

---
+++ Dwelling_2.kt
@@ -0,0 +1,12 @@
+fun main() {
+    
+}
+
+abstract class Dwelling(private var residents: Int) {
+    abstract val buildingMaterial: String
+    abstract val capacity: Int
+
+    fun hasRoom(): Boolean {
+        return residents < capacity
+    }
+}
